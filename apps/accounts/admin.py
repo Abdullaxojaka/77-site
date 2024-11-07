9671362a5f03f8506ec0d0eaa1e1b7718f31af50
unknown <berdiboyevavazbek43@gmail.com>
2024-11-07 18:31:51 +0300

    done

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User
+from django.contrib.auth.admin import Admin
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(Admin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Shaxsiy ma\'lumotlar', {'fields': ('first_name', 'last_name', 'email', 'phone_number', 'bio', 'profile_image')}),
+        ('Ruxsatlar', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        ('Muhim sanalar', {'fields': ('last_login', 'date_joined')}),
+    )
